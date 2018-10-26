f5a08e245db2c985b369a216a12a83e100196a28
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:16:36 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I99b7c60575e1f293f739e593f1db9ddf6e09cccb

--- NewUserMessage.php
+++ NewUserMessage.php
@@ -12,6 +12,11 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'NewUserMessage' );
 	$wgMessagesDirs['NewUserMessage'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for NewUserMessage extension. ' .
+		'Please use wfLoadExtension instead, see ' .
+		'https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 } else {
 	die( 'This version of the NewUserMessage extension requires MediaWiki 1.25+' );
 }
